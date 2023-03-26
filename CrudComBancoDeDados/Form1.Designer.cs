ade577eb2798f64981606561a5b88bc24149b20c
Pedro Candido Cardoso Neto <107777706+Pedrocandido162@users.noreply.github.com>
2023-03-25 21:07:58 -0300

    Add files via upload

---
+++ CrudComBancoDeDados/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace CrudComBancoDeDados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textSenha = new System.Windows.Forms.TextBox();
+            this.CadastrarButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.atualizarButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.ButtonExibir = new System.Windows.Forms.Button();
+            this.buttonConsulta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(29, 78);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(212, 20);
+            this.textEmail.TabIndex = 1;
+            this.textEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Senha";
+            // 
+            // textSenha
+            // 
+            this.textSenha.Location = new System.Drawing.Point(29, 136);
+            this.textSenha.Name = "textSenha";
+            this.textSenha.Size = new System.Drawing.Size(212, 20);
+            this.textSenha.TabIndex = 3;
+            this.textSenha.UseSystemPasswordChar = true;
+            this.textSenha.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // CadastrarButton
+            // 
+            this.CadastrarButton.Location = new System.Drawing.Point(29, 174);
+            this.CadastrarButton.Name = "CadastrarButton";
+            this.CadastrarButton.Size = new System.Drawing.Size(75, 23);
+            this.CadastrarButton.TabIndex = 4;
+            this.CadastrarButton.Text = "Cadastrar";
+            this.CadastrarButton.UseVisualStyleBackColor = true;
+            this.CadastrarButton.Click += new System.EventHandler(this.CadastrarButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(191, 174);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteButton.TabIndex = 5;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // atualizarButton
+            // 
+            this.atualizarButton.Location = new System.Drawing.Point(110, 174);
+            this.atualizarButton.Name = "atualizarButton";
+            this.atualizarButton.Size = new System.Drawing.Size(75, 23);
+            this.atualizarButton.TabIndex = 6;
+            this.atualizarButton.Text = "Atualizar";
+            this.atualizarButton.UseVisualStyleBackColor = true;
+            this.atualizarButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Id";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(29, 30);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(212, 20);
+            this.textId.TabIndex = 8;
+            this.textId.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.Location = new System.Drawing.Point(29, 220);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(468, 163);
+            this.dgvDados.TabIndex = 9;
+            this.dgvDados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ButtonExibir
+            // 
+            this.ButtonExibir.Location = new System.Drawing.Point(353, 174);
+            this.ButtonExibir.Name = "ButtonExibir";
+            this.ButtonExibir.Size = new System.Drawing.Size(75, 23);
+            this.ButtonExibir.TabIndex = 10;
+            this.ButtonExibir.Text = "Exibir";
+            this.ButtonExibir.UseVisualStyleBackColor = true;
+            this.ButtonExibir.Click += new System.EventHandler(this.textExibir_Click);
+            // 
+            // buttonConsulta
+            // 
+            this.buttonConsulta.Location = new System.Drawing.Point(272, 174);
+            this.buttonConsulta.Name = "buttonConsulta";
+            this.buttonConsulta.Size = new System.Drawing.Size(75, 23);
+            this.buttonConsulta.TabIndex = 11;
+            this.buttonConsulta.Text = "Consultar";
+            this.buttonConsulta.UseVisualStyleBackColor = true;
+            this.buttonConsulta.Click += new System.EventHandler(this.buttonConsulta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 429);
+            this.Controls.Add(this.buttonConsulta);
+            this.Controls.Add(this.ButtonExibir);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.atualizarButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.CadastrarButton);
+            this.Controls.Add(this.textSenha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textSenha;
+        private System.Windows.Forms.Button CadastrarButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button atualizarButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Button ButtonExibir;
+        private System.Windows.Forms.Button buttonConsulta;
+    }
+}
+
---
+++ CrudComBancoDeDados/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CrudComBancoDeDados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CadastrarButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;user=root;database=cadastro;password=;port=3306;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand comand = connection.CreateCommand();
+                comand.CommandText = "INSERT INTO pessoas (id, email, senha) VALUES (@id, @email, @senha)"; ;
+                comand.Parameters.AddWithValue("@id", textId.Text);
+                comand.Parameters.AddWithValue("@email", textEmail.Text);
+                comand.Parameters.AddWithValue("@senha", textSenha.Text);
+                comand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao cadastrar: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        /*private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }*/
+
+  
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;user=root;database=cadastro;password=;port=3306;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand comand = connection.CreateCommand();
+                comand.CommandText = "UPDATE pessoas SET email = @email, senha = @senha WHERE id = @id "; ;
+                comand.Parameters.AddWithValue("@email", textEmail.Text);
+                comand.Parameters.AddWithValue("@senha", textSenha.Text);
+                comand.Parameters.AddWithValue("@id", textId.Text);
+                comand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        private void textExibir_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;user=root;database=cadastro;password=;port=3306;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try {
+                connection.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM pessoas", connection);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvDados.DataSource = dt;
+            
+            } catch(Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            finally { connection.Close(); }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;user=root;database=cadastro;password=;port=3306;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand comand = connection.CreateCommand();
+                comand.CommandText = "DELETE FROM pessoas WHERE id =@id";
+                comand.Parameters.AddWithValue("@id",textId.Text);
+                comand.ExecuteNonQuery();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            finally { connection.Close(); }
+
+        }
+
+        private void buttonConsulta_Click(object sender, EventArgs e)
+        {
+                 string connectionString = "server=localhost;user=root;database=cadastro;password=;port=3306;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand comand = connection.CreateCommand();
+                comand.CommandText = "SELECT * FROM pessoas WHERE id =@id";
+                comand.Parameters.AddWithValue("@id",textId.Text);
+                MySqlDataReader dr;
+                dr = comand.ExecuteReader();
+                while (dr.Read())
+                {
+                    textEmail.Text = Convert.ToString(dr["email"]);
+                 
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+            finally { connection.Close(); }
+
+        
+        }
+    }
+}
+               /*MySqlCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM users";
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    MessageBox.Show(reader["name"] + " - " + reader["email"]);
+                }*/
